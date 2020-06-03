f73ddacb176a570ad4264800eeacc37a6a0a6a5a
Douglas Fernandes <douglasf.filho@gmail.com>
2020-06-03 12:32:27 -0300

    add: project creation

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Este é apenas um teste</Text>
+      <Text>Aguarde a notificação de login bem sucedido</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
